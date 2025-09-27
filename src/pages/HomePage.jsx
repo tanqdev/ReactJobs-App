afd703eeeb7a0413ddbf2ad53761c314ea9777d2
Tanishq Srivastava <srivastavatanishq142@gamil.com>
2025-09-28 04:32:39 +0530

    1

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobsListings from "../components/JobsListings";
+import ViewAllJobs from "../components/ViewAllJobs";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <JobsListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default HomePage;
